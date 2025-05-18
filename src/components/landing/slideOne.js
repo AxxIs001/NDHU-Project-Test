6338dd17094ada3bd0c89fe0bc62fbc2bd9dbb25
Chyngyz Abdikerimov <411021385@gms.ndhu.edu.tw>
2025-05-19 00:35:51 +0800

    Prepare for deploy

--- src/components/landing/slideOne.js
+++ src/components/landing/slideOne.js
@@ -16,13 +16,11 @@ const SlideOne = () => {
     return (
         <div className="flex flex-col items-center dark:bg-black ">
 
-            <h1 className="text-6xl font-black mt-20 max-md:text-3xl dark:text-white">Ai Course Recommendation and Assistance</h1>
-
+            <h1 className="text-6xl font-black mt-20 max-md:text-3xl text-center dark:text-white">
+            Ai Course Recommendation<br /><span className="block text-center">& Assistance</span>
+            </h1>
             <p className="text-center text-black mt-6 max-w-2xl font-medium max-md:text-xs dark:text-white">
-            Easily create personalized courses tailored to your needs.
-            Interact with an AI teacher chat, generate courses in 23+ languages, and test your knowledge with AI-generated quizzes.
-            Download courses for offline access and take notes as you go. Transform your learning experience today!
-            </p>
+            Explore honest student reviews, AI-powered suggestions , and everything you need to choose the right course - before it's too late</p>
 
             <div className="flex space-x-4 mb-4 mt-6">
                 <button onClick={redirectSignIn} className="border-black text-black border px-3 py-2 font-medium dark:border-white dark:text-white">
--- src/components/landing/slideThree.js
+++ src/components/landing/slideThree.js
@@ -10,24 +10,31 @@ const SlideThree = () => {
             </div>
             <div className="lg:flex py-24 max-md:py-14">
               
-                <div className="flex flex-col items-center justify-center lg:w-1/3 pt-6">
+                <div className="flex flex-col items-center lg:w-1/3 pt-6">
                     <PiKeyboard className="text-4xl max-md:text-3xl dark:text-white" />
                     <div className="text-2xl max-md:text-lg pt-4 font-bold  dark:text-white">Enter Course Title</div>
-                    <div className="text-lg max-md:text-xs text-center px-3 pt-2 font-medium  dark:text-white"  >Enter the course title for which you want to generate content using AI</div>
+                    <div className="text-lg max-md:text-xs text-center px-3 pt-2 font-medium  dark:text-white"  >Browse or search for university courses by name, department, category, or language.
+                    Quickly access syllabus, credit info, and professor details — all in one place</div>
                 </div>
 
          
-                <div className="flex flex-col items-center justify-center lg:w-1/3 pt-6">
+                <div className="flex flex-col items-center  lg:w-1/3 pt-6">
                     <RiAiGenerate  className="text-4xl max-md:text-3xl dark:text-white" />
-                    <div className="text-2xl max-md:text-lg pt-4 font-bold  dark:text-white">AI Generates Sub-Topic</div>
-                    <div className="text-lg max-md:text-xs text-center px-3 pt-2 font-medium  dark:text-white" >AI will generate topics and subtopics based on the title you provide</div>
+                    <div className="text-2xl max-md:text-lg pt-4 font-bold  dark:text-white">Explore Reviews & Ratings</div>
+                    <div className="text-lg max-md:text-xs text-center px-3 pt-2 font-medium  dark:text-white" >See real student feedback from those who took the course before you.
+                    Check comments about teaching style, difficulty level, and grading fairness.
+                </div>
                 </div>
 
                
-                <div className="flex flex-col items-center justify-center lg:w-1/3 pt-6">
+                <div className="flex flex-col items-center lg:w-1/3 pt-6">
                     <PiVideo   className="text-4xl max-md:text-3xl  dark:text-white" />
-                    <div className="text-2xl max-md:text-lg pt-4 font-bold  dark:text-white">Video & Theory Course</div>
-                    <div className="text-lg max-md:text-xs text-center px-3 pt-2 font-medium  dark:text-white" >AI will generate video and theory course allowing you to start learning</div>
+                    <div className="text-2xl max-md:text-lg pt-4 font-bold  dark:text-white">AI-Powered Learning Assistant
+
+                    </div>
+                    <div className="text-lg max-md:text-xs text-center px-3 pt-2 font-medium  dark:text-white" >AI assistant helps you learn more effectively by generating content based on your course syllabus — 
+                    or any topic you wish to explore.                   
+                    </div>
                 </div>
             </div>
         </div>
