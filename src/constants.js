4e89ff96c1e3f4249207e2f0e7ca2fa9c1868e80
Chyngyz Abdikerimov <411021385@gms.ndhu.edu.tw>
2025-07-12 23:57:52 +0800

    changes

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 export const name = 'NDHU Courses';
 export const company = 'NDHU';
-//export const websiteURL = 'http://localhost:3000';
-//export const serverURL = 'http://localhost:5000';
 export const websiteURL = 'https://university-management-bd099.web.app';
 export const serverURL = 'https://ndhu-project-test.onrender.com';
+//export const websiteURL = 'http://localhost:3000';
+//export const serverURL = 'http://localhost:5000';
 export const logo = '';
 
 //SOCIAL SIGNIN
